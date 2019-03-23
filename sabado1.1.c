361b61824a2c079b87c4325eb063d5b73f591dd0
carreracristian <carreracristian849@gmail.com>
2019-03-23 12:12:31 -0300

    sabado1

--- sabado1.1.c
+++ sabado1.1.c
@@ -1,23 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 int pedirNumero(void);
+float sacarPromedio(int suma,int cantidad);
+int sacarAprobacion(int notaMinima, int nota);
 int main()
 {
     int elNumeroIngresado;
-    elNumeroIngresado=pedirNumero();
-    printf("El numero ingresado fue:%d", elNumeroIngresado);
+    float promedio;
+    float promedioNoAprobados;
+    float promedioTotal;
+    int cantidadAprobados;
+    int estaAprobado;
+    int sumaTotal;
 
+    elNumeroIngresado=pedirNumero();
+    printf("\nEl numero ingresado fue:%d", elNumeroIngresado);
+    promedio = sacarPromedio(94,5);
+    printf("\nEl promedio fue: %f",promedio);
+    estaAprobado=sacarAprobacion(6,elNumeroIngresado);
+    if(estaAprobado==1)
+    {
+        printf("\naprobo");
+    }
+    else{
+        printf("\nno aprobo");
+    }
 
 }
 int pedirNumero(void)
 {
     int miNumero;
-    printf("Ingrese numero: ");
-    scanf("%d",&miNumero);
+    int numAlumnos;
+    numAlumnos=0;
 
-    while(miNumero<0 || miNumero>10)
+
+    while(numAlumnos<5)
     {
-        printf("Reingrese el numero: ");
-        scanf("%d", &miNumero);
+         printf("Ingrese la nota: ");
+         scanf("%d",&miNumero);
+
+        while(miNumero<0 || miNumero>10)
+            {
+                printf("Reingrese el numero: ");
+                scanf("%d", &miNumero);
+            }
+        numAlumnos++;
     }
 }
+float sacarPromedio(int suma, int cantidad)
+{
+   float retorno;
+   retorno=(float)suma/cantidad;
+   return retorno;
+
+}
+int sacarAprobacion(int notaMinima, int nota)
+{
+    int retorno;
+    int sumaAprobados;
+    int sumaNoAprobador;
+    sumaNoAprobador=0;
+    sumaAprobados=0;
+    if(nota>=notaMinima)
+    {
+        retorno=1;
+        sumaAprobados++;
+    }
+    else
+        {
+            retorno=0;
+            sumaNoAprobador++;
+            sumaNoAprobador=sumaNoAprobador+notaIngresada;
+        }
+        return retorno;
+}
+promedioTotal(sumaTotal,i);
+promedioNoAprobados
